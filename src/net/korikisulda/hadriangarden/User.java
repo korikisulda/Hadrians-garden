a6aa0a6591be96ebbf1130e98ddf3ac76f8e2b50
korikisulda <korikisulda@gmail.com>
2014-01-19 13:42:27 +0000

    Initial commit

---
+++ src/net/korikisulda/hadriangarden/User.java
@@ -0,0 +1,16 @@
+package net.korikisulda.hadriangarden;
+
+public class User {
+	
+	public String registerUser(){
+		return "";
+	}
+	
+	public boolean getUserStatus(){
+		return true;
+	}
+	
+	public void submitUrl(){
+		
+	}
+}
